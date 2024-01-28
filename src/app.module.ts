c7c9b60c7fe87c47ccba12d95a4c9074eae2ab8a
Amir <haghighi251@gmail.com>
2024-01-28 21:04:37 +0100

    feat: Expand GraphQL schemas into different files

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,15 +6,15 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 import { PetController } from './pet/pet.controller';
 import { PetModule } from './pet/pet.module';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
+// import { AuthModule } from './auth/auth.module';
+// import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       sortSchema: true,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      autoSchemaFile: join(process.cwd(), 'src/graphql/schemas/schema.gql'),
     }),
     PetModule,
   ],
